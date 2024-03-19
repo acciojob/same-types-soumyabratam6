f702e241fe6dfb07741a20eca8423de577ea97b9
soumyabratam6 <144446453+soumyabratam6@users.noreply.github.com>
2024-03-19 17:20:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function isSameType(value1, value2) {
    if (typeof(value1) == typeof(value2)) {
   	return true;
   } else if(Number.isNaN(value1) && Number.isNaN(value2)) {
-	  return false;
+	return false;
   }
   return false;
 }
